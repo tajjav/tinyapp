33de96a44ab1d60b1c416efe88c7f439c4f96abd
tajjav <taj.jav@hotmail.com>
2023-11-02 20:40:48 -0600

    this commit adds /urls/new route and urls_new.ejs template, form post request handler which print to console req.body

--- express_server.js
+++ express_server.js
@@ -2,26 +2,46 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = function(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+console.log(generateRandomString(6));
+
 app.set("view engine", "ejs");
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+
+// DB
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  //req.params.id = {"b2xVn2": "http://www.lighthouselabs.ca"};
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
-  //res.send(req.params);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
@@ -29,11 +49,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req,res) => {
-  const templateVars = { greeting: "Hello World!"};
-  //res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
