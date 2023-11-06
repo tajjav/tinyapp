23113697eb59e5641435585581e24d1b225d7712
tajjav <taj.jav@hotmail.com>
2023-11-05 17:23:46 -0700

    this commit adds edit long URL functionality along with post request handler.

--- express_server.js
+++ express_server.js
@@ -16,7 +16,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// Routes
+//-------------------------End Points-----------------------------//
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,9 +30,45 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// submit button on /urls/new
+app.post("/urls/new", (req, res) => {
+  const id = generateRandomString(6);
+  const value = req.body.longURL;
+  urlDatabase[id] = value;
+  res.redirect(`/urls/${id}`);
+});
+
+// edit button on /urls
+app.post("/urls/:id/edit", (req,res) => {
+  const shortUrlToEdit = req.url.split("/")[2];
+  const actionForShortUrl = req.url.split("/")[3];
+  res.redirect(`/urls/${shortUrlToEdit}`);
+});
+
+// delete button on /urls
+app.post("/urls/:id/delete", (req,res) => {
+  const shortUrlToDel = req.url.split("/")[2];
+  const actionForShortUrl = req.url.split("/")[3];
+  if (actionForShortUrl === 'delete') {
+    delete urlDatabase[shortUrlToDel];
+  }
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+// submit button on /urls/:id
+app.post("/urls/:id/edited", (req, res) => {
+  const shortUrlToEdit = req.url.split("/")[2];
+  const actionForShortUrl = req.url.split("/")[3];
+  const value =  req.body.editedLongURL;
+  if (actionForShortUrl === 'edited') {
+    urlDatabase[shortUrlToEdit] = value;
+  }
+  res.redirect("/urls");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -42,22 +78,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  const id = generateRandomString(6);
-  const value = req.body.longURL;
-  urlDatabase[id] = value;
-  res.redirect(`/urls/${id}`);
-});
-
-app.post("/urls/:id/delete", (req,res) => {
-  const shortUrlToDel = req.url.split("/")[2];
-  const actionForShortUrl = req.url.split("/")[3];
-  if (actionForShortUrl === 'delete') {
-    delete urlDatabase[shortUrlToDel];
-  }
-  res.redirect("/urls");
 });
 
 app.get("/u/:id", (req,res) => {
